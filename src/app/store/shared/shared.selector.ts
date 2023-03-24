01c3cf7c27ac7636a9f86ac49b2dfe161f653bf3
Nishanth Raghava Aitha <naitha@ctepl.com>
2023-03-24 16:43:55 +0530

    ngrx

--- src/app/store/shared/shared.selector.ts
+++ src/app/store/shared/shared.selector.ts
@@ -5,7 +5,5 @@ export const SHARED_STATE_NAME = 'shared'
 const getSharedState = createFeatureSelector<SharedState>(SHARED_STATE_NAME);
 
 export const getLoading = createSelector(getSharedState, (state) =>{
-    console.log(state);
-    
     return state.showLoading;
 });
